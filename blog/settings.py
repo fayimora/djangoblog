927edd189fd2182a7495e5d179446835f277e6bb
Fayimora Femi-Balogun <fayi@fayimora.com>
2017-01-15 01:41:19 +0000

    Setup core views :dancers:

--- blog/settings.py
+++ blog/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'blog.core',
     'blog.authentication',
 ]
 
@@ -56,7 +57,9 @@ ROOT_URLCONF = 'blog.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR + '/blog/templates/'
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- blog/urls.py
+++ blog/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
+from blog.core import views as core_views
+
 urlpatterns = [
+    url(r'^$', core_views.index, name='index'),
     url(r'^admin/', admin.site.urls),
 ]
